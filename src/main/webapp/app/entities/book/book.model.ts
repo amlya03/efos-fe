4be1f41b40a6f52dd2f5c20fbef5c6d4610b1871
adytia.m <adytia00maulana@gmail.com>
2022-09-08 16:34:15 +0700

    penambahan detail book

--- src/main/webapp/app/entities/book/book.model.ts
+++ src/main/webapp/app/entities/book/book.model.ts
@@ -7,8 +7,10 @@ export interface IBook {
   // contoh?: string | null;
   nama?: string | null;
   // kelas?: string | null;
-  // agama?: string | null;
+  agama?: string | null;
   app_no_de?: string | null;
+  program_pembiayaan?: string | null;
+  kategori_pekerjaan?: string | null;
 }
 
 export type NewBook = Omit<IBook, 'id'> & { id: null };
